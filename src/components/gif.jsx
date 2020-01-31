ac83f3db98127803da919508b7a0841441944db0
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-01-30 21:37:34 -0600

    Gif component completed

---
+++ src/components/gif.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+
+  render() {
+  const src = `https://media3.giphy.com/media/${this.props.id}/200.gif`
+    return(
+      <img src={src} alt="" className="gif"/>
+    );
+  }
+}
+
+export default Gif;
+
+
